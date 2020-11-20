9dad09b00cac2f5b970ed4c5d50a01836547b1c7
leo ouyang <Leo Ouyang>
2020-11-19 21:39:42 -0800

    Added a baseZ to Panels that is a value added to any update fucntions' z

--- glInterface.h
+++ glInterface.h
@@ -13,12 +13,14 @@ protected:
     glm::vec4 scale(const glm::vec4& scaleRect);
     glm::vec4 rect, backgroundColor;
     SpriteWrapper* sprite = nullptr;
+    double baseZ = 0; //will be added to the z passed in the update functions. In Windows, also added to child panels
 
 public:
-    Panel(const glm::vec4& rect_, const glm::vec4& bColor, SpriteWrapper* spr);
+    Panel(const glm::vec4& rect_, const glm::vec4& bColor, SpriteWrapper* spr, double z_ = 0);
     const glm::vec4& getRect();
     void changeRect(const glm::vec4& rect);
-    virtual void update(float mouseX,float mouseY, float z, const glm::vec4& scale); //mouseX and mouseY are the coordinates of the mouse, z is the z coordinate to render to,
+    virtual void update(float mouseX,float mouseY, float z, const glm::vec4& scale); //mouseX and mouseY are the coordinates of the mouse and should
+                                            //already be scaled and not have to be modified, z is the z coordinate to render to,
                                             //scale is NOT the rect that we want to project to; the x,y is the x-y increment to render to, and the z and a are the x and yscale respectively
     void update(float mouseX, float mouseY, float z); //same as other update function except we don't worry about scaling
     void updateBlit(float z, const glm::vec4& blit); //blits to a rect and updates
@@ -35,7 +37,7 @@ protected:
 public:
     using Panel::update;
 
-    Button(const glm::vec4& box, void (*func)(), SpriteWrapper* spr, const FontParameter& param, Font* font, const glm::vec4& color);
+    Button(const glm::vec4& box, void (*func)(), SpriteWrapper* spr, const FontParameter& param, Font* font, const glm::vec4& color, double z_ = 0);
     virtual void press();
     virtual void hover(); //what to do if the mouse hovers over this button
     virtual void render(bool mouseHover,float x = 0.0f, float y = 0.0f, float z = 0.0f, float xScale = 1, float yScale= 1); //x and y are the offset if we want to render the button relative to something
@@ -59,8 +61,8 @@ protected:
     std::vector<std::unique_ptr<Panel>> panels;
     bool doUpdate = true; //whether to update this window or not
 public:
-    Window( const glm::vec2& dimen, SpriteWrapper* spr, const glm::vec4& bg); //if window is supposed to be centered
-    Window( const glm::vec4& box, SpriteWrapper* spr, const glm::vec4& bg); //if window doesn't need to be centered
+    Window( const glm::vec2& dimen, SpriteWrapper* spr, const glm::vec4& bg, double z_ = 0); //if window is supposed to be centered
+    Window( const glm::vec4& box, SpriteWrapper* spr, const glm::vec4& bg, double z_ = 0); //if window doesn't need to be centered
     void setDoUpdate(bool val);
     bool getDoUpdate();
     int countPanels();
